c40b0c780a77a62a31ebd956470fc467cf02c3da
Tarsis <br.tmvdl@gmail.com>
2023-10-27 17:59:23 -0300

    Updating INDEX file

--- src/www/index.js
+++ src/www/index.js
@@ -1,34 +1,128 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 
 const __ = {
   getWidth: () => window.innerWidth,
   getHeight: () => window.innerHeight,
+  getSide: () => THREE.DoubleSide,
+  getPI: () => Math.PI / 2,
+}
+
+const planesInCube = [
+  [+0.0, +0.0, +0.5, +0.0, +0.0, +0.0,], // red
+  [+0.5, +0.0, +0.0, +0.0, +Math.PI / 2, +0.0,], // blue
+  [+0.0, +0.5, +0.0, +Math.PI / 2, +0.0, +0.0,], // white
+  [-0.5, +0.0, +0.0, +0.0, +Math.PI / 2, +0.0,], // green
+  [+0.0, +0.0, -0.5, +0.0, +0.0, +0.0,], // orange
+  [+0.0, -0.5, +0.0, -Math.PI / 2, +0.0, +0.0,], // yellow
+]
+
+const squaresInCube = [
+  [-1, -1, -1,],
+  [+0, -1, -1,],
+  [+1, -1, -1,],
+  [-1, +0, -1,],
+  [+0, +0, -1,],
+  [+1, +0, -1,],
+  [-1, +1, -1,],
+  [+0, +1, -1,],
+  [+1, +1, -1,],
+  [-1, -1, +0,],
+  [+0, -1, +0,],
+  [+1, -1, +0,],
+  [-1, +0, +0,],
+  [+0, +0, +0,],
+  [+1, +0, +0,],
+  [-1, +1, +0,],
+  [+0, +1, +0,],
+  [+1, +1, +0,],
+  [-1, -1, +1,],
+  [+0, -1, +1,],
+  [+1, -1, +1,],
+  [-1, +0, +1,],
+  [+0, +0, +1,],
+  [+1, +0, +1,],
+  [-1, +1, +1,],
+  [+0, +1, +1,],
+  [+1, +1, +1,],
+]
+
+const gui = new dat.GUI()
+
+const createFolder = (name = Date.now(), mesh = new THREE.Mesh()) => {
+  const folder = gui.addFolder(name)
+
+  folder.add(mesh.position, 'x', -Math.PI, Math.PI, Math.PI / 12).name('pos x')
+  folder.add(mesh.position, 'y', -Math.PI, Math.PI, Math.PI / 12).name('pos y')
+  folder.add(mesh.position, 'z', -Math.PI, Math.PI, Math.PI / 12).name('pos z')
+
+  folder.add(mesh.rotation, 'x', -Math.PI, Math.PI, Math.PI / 12).name('rot x')
+  folder.add(mesh.rotation, 'y', -Math.PI, Math.PI, Math.PI / 12).name('rot y')
+  folder.add(mesh.rotation, 'z', -Math.PI, Math.PI, Math.PI / 12).name('rot z')
 }
 
 const scene = new THREE.Scene()
+
 const camera = new THREE.PerspectiveCamera(75, __.getWidth() / __.getHeight(), 1e-1, 1e4)
-camera.position.set(2.5, 2.5, 2.5)
+camera.position.set(+2.5, +2.5, +2.5)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(__.getWidth(), __.getHeight())
 document.body.appendChild(renderer.domElement)
 document.body.style.margin = '0rem'
 
-scene.add(new THREE.GridHelper(10, 10))
+const createPlane = (color) => {
+  if (!color) throw new Error('It has no color!')
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x990099 }),
-)
+  return new THREE.Mesh(
+    new THREE.PlaneGeometry(+1.0, +1.0),
+    new THREE.MeshBasicMaterial({ color, side: __.getSide(), }),
+  )
+}
+
+const createSquare = (...colors) => {
+  if (colors.length != 6) throw new Error('It must to have 6 colors!')
+
+  const group = new THREE.Group()
+
+  colors.map((color, ix) => {
+    const plane = createPlane(color)
+    group.add(plane)
+    plane.position.set(planesInCube[ix][0], planesInCube[ix][1], planesInCube[ix][2],)
+    plane.rotation.set(planesInCube[ix][3], planesInCube[ix][4], planesInCube[ix][5],)
+
+    return plane
+  })
+
+  return group
+}
+
+const cube = new THREE.Group()
 scene.add(cube)
+
+Array.from(Array(27)).map((_, ix) => {
+  const sqare = createSquare(
+    0xff0000, // red
+    0x0000ff, // blue
+    0xffffff, // white
+    0x00ff00, // green
+    0xff9900, // orange
+    0xffff00, // yellow
+  )
+
+  sqare.position.set(
+    squaresInCube[ix][0] * +1.1,
+    squaresInCube[ix][1] * +1.1,
+    squaresInCube[ix][2] * +1.1,
+  )
+
+  cube.add(sqare)
+})
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-function animate() {
-  cube.rotation.x += 1e-2
-  cube.rotation.y += 1e-2
-
+const animate = () => {
   controls.update()
   renderer.render(scene, camera)
   requestAnimationFrame(animate)
